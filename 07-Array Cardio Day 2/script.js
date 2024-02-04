8de44e5ef5f7ec8924427d34197ba3008b13ad8e
Hilda Enyioko <enyiokohilda@gmail.com>
2024-02-04 16:22:30 +0100

    day 07 completed

--- 07-Array Cardio Day 2/script.js
+++ 07-Array Cardio Day 2/script.js
@@ -16,13 +16,26 @@
   ];
 
   // Some and Every Checks
+  const currentYear = (new Date()).getFullYear();
+
   // Array.prototype.some() // is at least one person 19 or older?
+  const isAdult = people.some(person => ((currentYear - person.year) >= 19));
+  console.log(isAdult);
+
   // Array.prototype.every() // is everyone 19 or older?
+  const allAdult = people.every(person => ((currentYear - person.year) >= 19));
+  console.log(allAdult);
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const thisComment = comments.find(comment => (comment.id === 823423));
+  console.log(thisComment);
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
+  const index = comments.findIndex(comment => (comment.id === 823423));
+  console.log(index);
   // delete the comment with the ID of 823423
+  comments.splice(index, 1);
+  console.log(comments);
