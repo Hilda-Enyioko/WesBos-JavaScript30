5c8cbe0a4a4b363bf392d5cba8ebc4ca6086c7c6
Hilda Enyioko <enyiokohilda@gmail.com>
2024-02-07 07:42:25 +0100

    day 09 files updated

---
+++ 09-Dev Tools Domination/script.js
@@ -0,0 +1,10 @@
+const dogs = [
+    {name: 'Snickers', age: 2},
+    {name: 'Hugo', age: 8}
+];
+
+function makeGreen() {
+    const p = document.getElementsByName('p');
+    p.style.color = "#BADA55";
+    p.style.fontSize = '50px';
+}
