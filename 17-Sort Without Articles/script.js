b8d90d87f6a8a25be285d4c71a02d1ec3ee07efa
Hilda Enyioko <enyiokohilda@gmail.com>
2024-02-24 01:29:46 +0100

    day 17 completed

--- 17-Sort Without Articles/script.js
+++ 17-Sort Without Articles/script.js
@@ -1,1 +1,14 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function trim(a) {
+    return a.replace(/^(a |an |the )/i, "");
+}
+
+const sortedBands = bands.sort((a, b) => {
+    return trim(a) > trim(b) ? 1 : -1;
+});
+console.log(sortedBands);
+
+const list = document.querySelector('#bands');
+
+list.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
