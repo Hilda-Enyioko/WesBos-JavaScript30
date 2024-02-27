1b0b0a14b88dde38a390ed7df8733999ffa3e005
Hilda Enyioko <enyiokohilda@gmail.com>
2024-02-27 23:10:01 +0100

    day 20 completed

--- 20-Speech Detection/script.js
+++ 20-Speech Detection/script.js
@@ -1,1 +1,28 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const words = document.querySelector('.words');
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+words.appendChild(p);
+
+recognition.addEventListener('result', (e) => {
+    Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
+    if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+    }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
+
