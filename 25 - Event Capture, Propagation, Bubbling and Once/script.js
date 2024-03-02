0cdc67e4447afc7652758a23023fa980248d3044
Hilda Enyioko <enyiokohilda@gmail.com>
2024-03-02 14:27:39 +0100

    complete

--- 25 - Event Capture, Propagation, Bubbling and Once/script.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -3,13 +3,14 @@ const button = document.querySelector('button');
 
 function logText(e) {
     console.log(this.classList.value);
-    // e.stopPropagation(); // stop bubbling!
+    // e.stopPropagation(); 
+    // stop bubbling!
     // console.log(this);
 }
 
 divs.forEach(div => div.addEventListener('click', logText, {
     capture: false,
     once: true
-  }));
+}));
 
 button.addEventListener('click', () => {console.log('clicked !!!')});
