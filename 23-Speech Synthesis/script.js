e53b66f547ca13ece2145f865d356de03f77eaf8
Hilda Enyioko <enyiokohilda@gmail.com>
2024-03-02 13:22:30 +0100

    day 23 completed

--- 23-Speech Synthesis/script.js
+++ 23-Speech Synthesis/script.js
@@ -4,3 +4,34 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter((voice) => { return voice.lang.includes('en');})
+    .map((voice) => {return `<option value="${voice.name}">${voice.name} ${voice.lang}</option>`})
+    .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name = this.value);
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  console.log(msg[this.name]);
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
